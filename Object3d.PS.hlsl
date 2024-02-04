0f1871ae35ced8e0174004d3c818a6d6b7c74f37
sakamoto-ko <115054832+sakamoto-ko@users.noreply.github.com>
2024-02-04 21:40:58 +0900

    プロジェクト ファイルを追加します。

---
+++ Object3d.PS.hlsl
@@ -0,0 +1,69 @@
+#include "Particle.hlsli"
+
+struct Material
+{
+    float32_t4 color;
+    int32_t enableLighting;
+    float32_t4x4 uvTransform;
+    float32_t shininess;
+};
+
+struct Camera
+{
+    float32_t3 worldPosition;
+};
+
+struct DirectionalLight
+{
+    float32_t4 color;
+    float32_t3 direction;
+    float intensity;
+};
+
+ConstantBuffer<Material> gMaterial : register(b0);
+
+ConstantBuffer<Camera> gCamera : register(b2);
+
+Texture2D<float32_t4> gTexture : register(t0);
+
+SamplerState gSampler : register(s0);
+
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+
+    float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
+    
+    float32_t3 reflectLight = reflect(normalize(gDirectionalLight.direction), normalize(input.normal));
+    
+    float RdotE = dot(reflectLight, toEye);
+    float specularPow = pow(saturate(RdotE), gMaterial.shininess);
+    
+    if (gMaterial.enableLighting != 0)
+    {
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+    
+        float32_t3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+    
+        float32_t3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float32_t3(1.0f, 1.0f, 1.0f);
+
+        output.color.rgb = diffuse + specular;
+        
+        output.color.a = gMaterial.color.a * textureColor.a;
+    }
+    else
+    {
+        output.color = gMaterial.color * textureColor;
+    }
+    
+    return output;
+}
